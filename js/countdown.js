d88c7e01c56dcc5b782f2a563967f75445c1980e
ismaoresss <ismael.oreste@alumnos.frgp.utn.edu.ar>
2025-02-27 20:15:03 -0300

    +

--- js/countdown.js
+++ js/countdown.js
@@ -1,9 +1,9 @@
 simplyCountdown('#cuenta', {
 	year: 2025, // required
-	month: 2, // required
-	day: 27, // required
-	hours: 15, // Default is 0 [0-23] integer
-	minutes: 17, // Default is 0 [0-59] integer
+	month: 4, // required
+	day: 11, // required
+	hours: 0, // Default is 0 [0-23] integer
+	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
